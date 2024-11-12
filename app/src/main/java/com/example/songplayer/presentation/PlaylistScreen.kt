693d7738c29afd41a7812b0b15a454c755eadc8d
PoPaDoSs14 <slasevvladimir7@gmail.com>
2024-11-12 18:47:48 +0300

    add new design MusicItem

--- app/src/main/java/com/example/songplayer/presentation/PlaylistScreen.kt
+++ app/src/main/java/com/example/songplayer/presentation/PlaylistScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
@@ -26,6 +27,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.example.songplayer.domain.Music
@@ -71,19 +73,22 @@ fun MusicItem(music: Music) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)
-            .background(MaterialTheme.colorScheme.primary)
+            .padding(vertical = 24.dp, horizontal = 16.dp)
+            .background(MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(12.dp))
             .clickable { },
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
             text = music.name,
-            style = MaterialTheme.typography.bodyLarge,
-            modifier = Modifier.weight(1f)
+            color = Color.White,
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 16.dp)
         )
         Text(
             text = music.artist,
-            style = MaterialTheme.typography.bodySmall
+            style = MaterialTheme.typography.bodyLarge.copy(color = Color.White),
+            modifier = Modifier.padding(start = 16.dp)
         )
     }
 }
