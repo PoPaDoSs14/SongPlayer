c6d4772ec98886bbf0e5dc919954907969161c6e
PoPaDoSs14 <slasevvladimir7@gmail.com>
2024-11-20 20:41:28 +0300

    add new design PlaylistScreen

--- app/src/main/java/com/example/songplayer/presentation/PlaylistScreen.kt
+++ app/src/main/java/com/example/songplayer/presentation/PlaylistScreen.kt
@@ -8,12 +8,16 @@ import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -31,6 +35,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.songplayer.domain.Music
@@ -45,7 +50,6 @@ fun PlaylistScreen(viewModel: PlaylistViewModel, navHostController: NavHostContr
         onResult = { uri: Uri? ->
             uri?.let {
                 viewModel.addMusic(it)
-
             }
         }
     )
@@ -77,30 +81,37 @@ fun PlaylistScreen(viewModel: PlaylistViewModel, navHostController: NavHostContr
     }
 }
 
-@Composable
-fun MusicItem(music: Music, navController: NavHostController) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 24.dp, horizontal = 16.dp)
-            .background(MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(12.dp))
-            .clickable {
-
-                navController.navigate("MusicPlayerScreen/${music.id}") // Используем id или любую уникальную характеристику
-            },
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            text = music.name,
-            color = Color.White,
+    @Composable
+    fun MusicItem(music: Music, navController: NavHostController) {
+        Row(
             modifier = Modifier
-                .weight(1f)
-                .padding(end = 16.dp)
-        )
-        Text(
-            text = music.artist,
-            style = MaterialTheme.typography.bodyLarge.copy(color = Color.White),
-            modifier = Modifier.padding(start = 16.dp)
-        )
+                .fillMaxWidth()
+                .padding(vertical = 16.dp, horizontal = 12.dp)
+                .background(MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(12.dp))
+                .clickable {
+                    navController.navigate("MusicPlayerScreen/${music.id}")
+                }
+                .animateContentSize(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(
+                modifier = Modifier.weight(1f)
+                    .padding(start = 8.dp)
+            ) {
+                Text(
+                    text = music.name,
+                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = music.artist,
+                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        }
     }
-}
